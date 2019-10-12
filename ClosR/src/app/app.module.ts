21586767966f244937b97021c4e02af5a192d381
Vital Vercleyen <Vital.Vercleyen@realdolmen.com>
2019-10-12 12:03:56 +0200

    lil layout

--- ClosR/src/app/app.module.ts
+++ ClosR/src/app/app.module.ts
@@ -4,16 +4,37 @@ import { RouterModule, Routes } from "@angular/router";
 
 import { AppComponent } from "./app.component";
 import { SellingProductsComponent } from "./components/selling-products/selling-products.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { SellingProductComponent } from "./components/selling-product/selling-product.component";
+import { MatCardModule } from "@angular/material/card";
+import { MatButtonModule } from "@angular/material/button";
+import { HomeComponent } from "./components/home/home.component";
 
 const routerConfig: Routes = [
+  {
+    path: "",
+    component: HomeComponent
+  },
+
   {
     path: "products",
     component: SellingProductsComponent
   }
 ];
 @NgModule({
-  declarations: [AppComponent, SellingProductsComponent],
-  imports: [BrowserModule, RouterModule.forRoot(routerConfig)],
+  declarations: [
+    AppComponent,
+    SellingProductsComponent,
+    SellingProductComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routerConfig),
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
